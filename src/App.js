34afbbc572227bd4422b9bf1ab618f3d07998f42
Ngoc <bangoc.gdg@gmail.com>
2022-09-12 09:32:21 +0700

    Training State + use effect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component }  from 'react';
+import React, { Component, useState, useEffect }  from 'react';
 
 const myElement = <Car brand="Ford" />;
 
@@ -16,27 +16,91 @@ function Bike() {
   return <h2>Hi, I am a Bike!</h2>;
 }
 
-
 function Button(props) {
   function activateFeedbackOnClick(){
     return <h2>Hi, I am a Bike!</h2>;
   }
-  return <button onClick={(e) => activateFeedbackOnClick()}>{props.feedback}</button>
+  return <button onClick={(e) => activateFeedbackOnClick()}>{props.feedback} {props.count}</button>
 }
 
 function App() {
   // const y = z.map(item => <Car brand={item} />)
+  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState('Hi');
+
+  const [checkpoint1, setCheckpoint1] = useState(0);
+  const [checkpoint2, setCheckpoint2] = useState(0);
+
+
+  useEffect(() => {
+    if (count === 10) {
+      alert('Go 1')
+      setCheckpoint1(10)
+    }
+  }, [count])
+
+
+  useEffect(() => {
+    if (checkpoint1 === 10) {
+      setCheckpoint2(20)
+      alert('Go 2')
+    }
+  }, [checkpoint1])
+
+  
+  const [materials, setMaterials] = useState([])
+
+  // useEffect(() => {
+  //   // listmaterials
+
+  //   listmaterials().then((res) => {
+  //     if (res.status === 200) {
+  //       setMaterials(res.data || []);
+  //     }
+  //   })
+  // }, [])
+
+
+  // useEffect(() => {
+  //   if (materials.length > 0) {
+  //     // Call api to get material status.
+
+  //   }
+  // }, [materials])
+
+
+  useEffect(() => {
+    console.log('---go')
+  }, [])
+
   const button = b.map(item => <Button feedback={item} />)
+
   let x = <div>
-      {/* <p>Hello world</p>
-      <img src="https://dictionary.cambridge.org/vi/images/thumb/sports_noun_002_35307.jpg?version=5.0.250" alt="Italian Trulli"></img>
-      <a href="https://protonx.io/">protonx</a>
-      {/* <Car brand="Ford" /> */}
-      {/* <Car brand="Toyota"/> */}
-      {/* {y} */}
       {button}
     </div>
-  return x
+
+  const plusOnClick = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+    setMessage(`I am ${count}`)
+  }
+
+
+  const displayMessage = () => {
+    if (count > 1) {
+      return <>Better than 1</>
+    }
+  }
+
+  return <>
+    {count}
+    <button onClick={() => plusOnClick()}> Plus </button>
+    <Button count={count} feedback='Hello' />
+    <div>
+      {message}
+    </div>
+    {displayMessage()}
+  </>
 }
 
 export default App;
